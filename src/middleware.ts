394e13bf9cbffc2bbd530ee132b0dbdd1d9657af
zachgumball <harryzynext@gmail.com>
2025-03-11 21:51:13 +0700

    coba dulu

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { getToken } from "next-auth/jwt";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET, secureCookie: process.env.NODE_ENV === "production" });
 
   console.log("🔍 Middleware Debugging:");
   console.log("Path:", req.nextUrl.pathname);
